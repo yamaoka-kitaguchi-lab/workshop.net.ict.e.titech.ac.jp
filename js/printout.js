9548d689271789cf18546c18185460d36894dde9
mi2428 <mi2428782020@gmail.com>
2019-03-23 15:35:00 +0900

    Manual deploy (Add button to printout only program)
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

---
+++ js/printout.js
@@ -0,0 +1,10 @@
+const hide_elements = $('#announce,#print_program,#detail,#update,#footer');
+const bg_image = $('body').css('background-image');
+
+$('#print_program').click(() => {
+  hide_elements.addClass('d-print-none');
+  $('body').css('background-image', 'none');
+  window.print();
+  hide_elements.removeClass('d-print-none');
+  $('body').css('background-image', bg_image);
+});
